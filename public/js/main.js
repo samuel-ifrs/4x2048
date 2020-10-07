a38454b1b75f78c26adc210d27dda2c40d99108e
Samuel Andrade <gadeji@gmail.com>
2020-10-07 06:02:59 +0000

    Limpeza do código

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,10 @@
 import createBoard from "./board.js";
 
 window.boards = [];
-
 window.onload = ()=>{
-
     for(let i=0;i<4;i++){
         const board = createBoard();
         window.boards.push(board);
         document.getElementById('game').appendChild(board.element);
-    } 
+    }
 };
-
-window.teste = () => {
-    let passos = [
-        () => boards[0].newTile(1,1,1),
-        () => boards[0].newTile(0,1,1),
-        () => boards[0].newTile(0,0,2),
-        () => boards[0].moveTile(1, 1, 0, 1),
-        () => boards[0].moveTile(0, 1, 0, 0),
-        () => boards[0].moveTile(0, 0, 0, 1),
-        () => boards[0].moveTile(0, 1, 1, 1)
-    ];
-    console.log('[..] Teste automatizado');
-    (function fazTeste(intervalo = 500) {
-        const passo = passos.shift();
-        console.log(" [->]", passo, "\n [<-] ");
-        console.log(passo());
-        if(passos.length > 0)
-            setTimeout(fazTeste, intervalo);
-        else
-            console.log('[OK] Teste concluído');
-    })()
-}
