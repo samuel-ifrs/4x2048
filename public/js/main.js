b8d8c873d678135c03f35b65c31bde6b112abf61
Samuel Andrade <gadeji@gmail.com>
2020-10-07 04:16:05 +0000

    Função para criar blocos

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+window.boards = [];
 window.onload = ()=>{
     function createBoard(divBoard){
         const options = {
@@ -5,16 +6,39 @@ window.onload = ()=>{
             tileSize: '64px',
             borderSize: '10px',
             borderColor: 'rgb(187, 173, 160)',
-            baseColor: 'rgba(255, 255, 255, 0.5)',
+            baseColor: '#ffffff40'
         };
         const board = {};
+        board.options = options;
         board.element = divBoard;
+        board.tiles = [];
+        function setTile(posX, posY, object, value){
+            const index = posY*options.boardSize + posX;
+            object.style.setProperty('--posX', posX);
+            object.style.setProperty('--posY', posY);
+            object.setAttribute('value', value);
+            board.tiles[index] = {posX, posY, value, object};
+        }
+        function newTile(posX, posY, value = 1){
+            const index = posY*options.boardSize + posX;
+            let object;
+            if(board.tiles[index])
+                object = board.tiles[index].object;
+            else {
+                object = document.createElement('div');
+                object.className = 'tile';
+                board.element.appendChild(object);
+            }
+            setTile(posX, posY, object, value);
+        }
+        board.newTile = newTile;
         for(const opt in options){
             board.element.style.setProperty(`--${opt}`,options[opt]);
         }
         for(let i=0; i<options.boardSize**2; i++){
             let ix = i % parseInt(options.boardSize);
             let iy = Math.trunc(i / options.boardSize);
+            board.tiles[i] = null;
             let ghostTile = document.createElement('div');
             ghostTile.className = 'ghostTile tile';
             ghostTile.style.setProperty('--posX', ix);
@@ -25,8 +49,8 @@ window.onload = ()=>{
         return board;
     }
 
-    let board1 = createBoard(document.getElementById('board1'));
-    let board2 = createBoard(document.getElementById('board2'));
-    let board3 = createBoard(document.getElementById('board3'));
-    let board4 = createBoard(document.getElementById('board4'));
+    window.boards.push(createBoard(document.getElementById('board1')));
+    window.boards.push(createBoard(document.getElementById('board2')));
+    window.boards.push(createBoard(document.getElementById('board3')));
+    window.boards.push(createBoard(document.getElementById('board4')));
 };
